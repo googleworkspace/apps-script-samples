0efac264ceb76081722560c84ac16248f19b2164
Steven Bazyl <sbazyl@google.com>
2022-04-11 13:59:35 -0600

    Add workflow to publish projects to apps script when merged.

---
+++ .github/scripts/clasp_push.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+dirs=()
+
+IFS=$'\n' read -r -d '' -a dirs < <( find . -name 'build.gradle' -exec dirname '{}' \; | sort -u )
+
+exit_code=0
+
+for dir in "${dirs[@]}"; do
+  pushd "${dir}" || exit
+  clasp push -f
+  status=$?
+  if [ $status -ne 0 ]; then
+    exit_code=$status
+  fi
+  popd || exit
+done
+
+if [ $exit_code -ne 0 ]; then
+  echo "Script push failed."
+fi
+
+exit $exit_code
